1156dc6950692baef5668c29166b5e92df95c178
frransier <frransier@gmail.com>
2019-09-23 23:42:22 +0200

    leaderboard working yay

--- site/src/pages/leaderboard.js
+++ site/src/pages/leaderboard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useReducer, useEffect } from "react"
 import { graphql } from "gatsby"
 import Image from "gatsby-image"
 import { useGraphQL } from "@brightleaf/react-hooks"
@@ -6,6 +6,7 @@ import { mapEdgesToNodes } from "../helpers"
 import { Flex, Box, Text, Card, Heading } from "rebass"
 import { GiSoccerBall } from "react-icons/gi"
 import Layout from "../components/layout"
+import userReducer from "../state/userReducer"
 
 export const query = graphql`
   query Logos {
@@ -45,19 +46,18 @@ const LeaderboardPage = props => {
   }
 }`
   )
+  const [state, dispatch] = useReducer(userReducer, null)
+  useEffect(() => {
+    if (data) {
+      dispatch({ type: "sort", data: data.players })
+    }
+  }, [data])
 
   const images = mapEdgesToNodes(props.data.teamLogos)
   const getLogo = id => {
     const logo = images.find(x => x._id === id)
     const fixed = logo.logo.asset.fixed
     return fixed
-  }
-
-  const getPoints = playerList => {
-    const points = playerList.map(p => p.matchPoints)
-    const total = points.reduce((a, b) => a + b, 0)
-
-    return total
   }
 
   if (loading) {
@@ -73,57 +73,59 @@ const LeaderboardPage = props => {
   if (error) {
     return <Box>Error getting graphql data</Box>
   }
+
   return (
     <Layout>
       <Card textAlign="center">
         <Heading>Leaderboard</Heading>
       </Card>
-      {data.players.map(p => (
-        <Card key={p._id} width="100%">
-          <Flex key={p._id}>
-            {p.players.map(player => {
-              return (
-                <Card
-                  sx={{
-                    width: "20%",
-                    mx: "auto",
-                  }}
-                  key={player._id}
-                  fontSize="0"
-                >
-                  <Text textAlign="center" height="30px" mt={1}>
-                    {player.name}
-                  </Text>
-                  <Box textAlign="center">
-                    <Image
-                      key={player._id}
-                      fixed={getLogo(player.team._id)}
-                    ></Image>
-                  </Box>
-                  <Box textAlign="center" mt={2}>
-                    <Text>{player.matchGoals || 0} Mål</Text>
-                    <Text>{player.matchAssists || 0} Ass</Text>
-                  </Box>
-                </Card>
-              )
-            })}
-          </Flex>
-          <Card fontSize={0} textAlign="right">
-            <Flex>
-              {[...Array(getPoints(p.players))].map((_, i) => (
-                <Box key={i} mx={1} color="primary">
-                  <GiSoccerBall size={25}></GiSoccerBall>
-                </Box>
-              ))}
-              <Box mx="auto"></Box>
-              <Box textAlign="right">
-                <Text>{`id: ${p._id.substring(16, 25)}`}</Text>
-                <Text>{`points: ${getPoints(p.players)}`}</Text>
-              </Box>
+      {state &&
+        state.map(p => (
+          <Card key={p._id} width="100%">
+            <Flex key={p._id}>
+              {p.players.map(player => {
+                return (
+                  <Card
+                    sx={{
+                      width: "20%",
+                      mx: "auto",
+                    }}
+                    key={player._id}
+                    fontSize="0"
+                  >
+                    <Text textAlign="center" height="30px" mt={1}>
+                      {player.name}
+                    </Text>
+                    <Box textAlign="center">
+                      <Image
+                        key={player._id}
+                        fixed={getLogo(player.team._id)}
+                      ></Image>
+                    </Box>
+                    <Box textAlign="center" mt={2}>
+                      <Text>{player.matchGoals || 0} Mål</Text>
+                      <Text>{player.matchAssists || 0} Ass</Text>
+                    </Box>
+                  </Card>
+                )
+              })}
             </Flex>
+            <Card fontSize={0} textAlign="right">
+              <Flex>
+                {[...Array(p.score)].map((_, i) => (
+                  <Box key={i} mx={1} color="primary">
+                    <GiSoccerBall size={25}></GiSoccerBall>
+                  </Box>
+                ))}
+                <Box mx="auto"></Box>
+                <Box textAlign="right">
+                  <Text>{`id: ${p._id.substring(16, 25)}`}</Text>
+                  <Text>{`points: ${p.score}`}</Text>
+                </Box>
+              </Flex>
+            </Card>
           </Card>
-        </Card>
-      ))}
+        ))}
     </Layout>
   )
 }
