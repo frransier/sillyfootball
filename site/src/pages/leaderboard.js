4e705a33cb691b0afdf88644f02d8995fece05c8
frransier <frransier@gmail.com>
2019-09-28 13:05:47 +0200

    track teams on leaderboard

--- site/src/pages/leaderboard.js
+++ site/src/pages/leaderboard.js
@@ -1,14 +1,16 @@
-import React, { useReducer, useEffect } from "react"
+import React, { useReducer, useEffect, useState } from "react"
 import { graphql } from "gatsby"
 import Image from "gatsby-image"
 import { useGraphQL } from "@brightleaf/react-hooks"
 import { mapEdgesToNodes } from "../helpers"
-import { Flex, Box, Text, Card } from "rebass"
+import { Button, Flex, Box, Text, Card, Heading } from "rebass"
+import { Label, Input } from "@rebass/forms"
 import { GiSoccerBall } from "react-icons/gi"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import userReducer from "../state/userReducer"
 import { motion } from "framer-motion"
+import { push_uniq } from "terser"
 
 export const query = graphql`
   query Logos {
@@ -30,11 +32,14 @@ export const query = graphql`
 `
 
 const LeaderboardPage = props => {
+  const [email, setEmail] = useState()
+  const [trackedTeams, setTrackedTeams] = useState([])
   const { data, loading, error } = useGraphQL(
     "https://0jt5x7hu.api.sanity.io/v1/graphql/even/default",
     `query Leaderboard {
       players: allUsers {
         _id
+        email
         players {
           name
           _id
@@ -69,6 +74,11 @@ const LeaderboardPage = props => {
 
       return topScorers.length
     }
+  }
+
+  const trackTeams = () => {
+    const filter = state.filter(x => x.email === email)
+    setTrackedTeams(filter)
   }
 
   if (loading) {
@@ -107,6 +117,78 @@ const LeaderboardPage = props => {
             )}
           </Flex>
         )}
+      </Card>
+      <Card>
+        <Flex alignItems="center" justifyContent="center" width={1}>
+          <Label mx={1} width={1 / 3} htmlFor="trackTeams">
+            Visa dina lag
+          </Label>
+          <Input
+            id="trackTeams"
+            name="trackTeams"
+            type="trackTeams"
+            placeholder="glenn@gbg.nu"
+            onChange={event => setEmail(event.target.value)}
+          />
+          <Button ml={2} onClick={trackTeams}>
+            Go
+          </Button>
+        </Flex>
+      </Card>
+      {trackedTeams.length > 0 &&
+        trackedTeams.map(p => (
+          <Card key={p._id} width="100%">
+            <Text my={1} fontSize={1} fontWeight="bold">
+              Ditt lag: {p.email}
+            </Text>
+            <Flex>
+              {p.players.map(player => {
+                return (
+                  <Card
+                    sx={{
+                      width: "20%",
+                      mx: "auto",
+                    }}
+                    key={player._id}
+                    fontSize="0"
+                  >
+                    <Text textAlign="center" height="30px" mt={1}>
+                      {player.name}
+                    </Text>
+                    <Box textAlign="center">
+                      <Image
+                        key={player._id}
+                        fixed={getLogo(player.team._id)}
+                        alt={player.name}
+                      ></Image>
+                    </Box>
+                    <Box textAlign="center" mt={2}>
+                      <Text>{player.matchGoals || 0} Mål</Text>
+                      <Text>{player.matchAssists || 0} Ass</Text>
+                    </Box>
+                  </Card>
+                )
+              })}
+            </Flex>
+            <Card fontSize={0} textAlign="right">
+              <Flex>
+                {[...Array(p.score)].map((_, i) => (
+                  <Box key={i} mx={1} color="primary">
+                    <GiSoccerBall size={25}></GiSoccerBall>
+                  </Box>
+                ))}
+                <Box mx="auto"></Box>
+                <Flex textAlign="right">
+                  <Box mx={2} fontWeight="bold" fontSize={2}>
+                    <Text>{`Poäng: ${p.score}`}</Text>
+                  </Box>
+                </Flex>
+              </Flex>
+            </Card>
+          </Card>
+        ))}
+      <Card>
+        <Heading>Highscore</Heading>
       </Card>
       {state &&
         state.map(p => (
