b8ba7db09cbf4163f3d52bca5899100237d6b853
frransier <frransier@gmail.com>
2019-10-02 23:24:41 +0200

    test

--- site/functions/register/register.js
+++ site/functions/register/register.js
@@ -13,7 +13,7 @@ exports.handler = (event, _, callback) => {
   var body = JSON.parse(event.body)
   var data = body.params
   console.log(JSON.stringify(data.email))
-  const datum = Date.now() / 1000
+  const datum = Math.floor(Date.now() / 1000)
 
   const players = data.squad.map(player => {
     const p = { _ref: player, _key: player, _type: "reference" }
