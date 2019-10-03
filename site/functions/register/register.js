4302fd06b7e80cc599c507f087a06416b31724c7
frransier <frransier@gmail.com>
2019-10-03 20:43:09 +0200

    test

--- site/functions/register/register.js
+++ site/functions/register/register.js
@@ -22,37 +22,83 @@ exports.handler = (event, _, callback) => {
 
   const doc = {
     _type: "user",
-
     email: data.email,
     createdAt: new Date(),
     players: players,
+  }
+
+  const message = {
+    message_type: "email",
+    subject: "Du är reggad",
+    template: "personal",
+    body:
+      " " +
+      "<html>  " +
+      "<body>  " +
+      "<h1>  " +
+      "<b> Spelstart <mark>Lördag kl 13.25</mark> </b> " +
+      "</h1> " +
+      "<p>Brighton - Tottenham<br>  " +
+      "</p>  " +
+      "<h2>  " +
+      "Ditt lag:" +
+      "</h2>  " +
+      "<ul>  " +
+      `<li>${data.squad[0].name}</li>  ` +
+      `<li>${data.squad[1].name}</li>  ` +
+      `<li>${data.squad[2].name}</li>  ` +
+      `<li>${data.squad[3].name}</li>  ` +
+      `<li>${data.squad[4].name}</li>  ` +
+      "</ul>  " +
+      "<h2>  " +
+      "</body>  " +
+      "</html>",
+    from: {
+      type: "admin",
+      email: "hello@sillyfootball.se",
+    },
+    to: {
+      type: "user",
+      email: data.email,
+    },
   }
 
   try {
     sanity.create(doc)
     console.log("Player registered ")
 
-    intercom.users.create({ email: data.email }).then(() => {
-      intercom.events
-        .create({
-          event_name: "Team created",
-          created_at: datum,
-          email: data.email,
-          metadata: {
-            player_1: data.squad[0].name,
-            player_2: data.squad[1].name,
-            player_3: data.squad[2].name,
-            player_4: data.squad[3].name,
-            player_5: data.squad[4].name,
-          },
-        })
-        .then(() => {
-          console.log("Success")
-        })
-    })
-
-    console.log("did it work?")
-
+    intercom.users
+      .create({
+        email: data.email,
+        lastSeen: datum,
+      })
+      .then(() => {
+        intercom.events
+          .create({
+            event_name: "Team created",
+            created_at: datum,
+            email: data.email,
+            metadata: {
+              player_1: data.squad[0].name,
+              player_2: data.squad[1].name,
+              player_3: data.squad[2].name,
+              player_4: data.squad[3].name,
+              player_5: data.squad[4].name,
+            },
+          })
+          .then(() => {
+            console.log("Success")
+          })
+          .then(() => {
+            intercom.messages
+              .create({
+                message,
+              })
+              .then(() => {
+                console.log("Sent email to: ", data.email)
+              })
+          })
+      })
     callback(null, {
       statusCode: 200,
       body: `OK`,
