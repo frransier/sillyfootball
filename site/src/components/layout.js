86a860b57a756b13287c5229e90f96a979827016
frransier <frransier@gmail.com>
2019-10-12 11:31:04 +0200

    test

--- site/src/components/layout.js
+++ site/src/components/layout.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Box } from "rebass"
 import Header from "./header"
+import Intercom from "react-intercom"
 
 const Layout = ({ children }) => {
   return (
     <>
       <Box mx="auto" maxWidth={960} p={1}>
+        <Intercom appId="entknhmw"></Intercom>
         <Header />
         <main>{children}</main>
       </Box>
