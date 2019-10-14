8bc7b9ecaa49a1cce2cb975fb45ce0b744809cc1
frransier <frransier@gmail.com>
2019-10-14 21:58:10 +0200

    new logo

--- site/src/components/seo.js
+++ site/src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import logo from "../images/logo.png"
+import logo from "../images/logo.webp"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
