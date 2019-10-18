818ee0e0d00cbfe7098bfd30988624e4a4448062
frransier <frransier@gmail.com>
2019-10-18 18:22:12 +0200

    scroll bar solved

--- site/src/components/layout.js
+++ site/src/components/layout.js
@@ -1,10 +1,18 @@
 import React from "react"
 import { Box } from "rebass"
 import Header from "./header"
+import { Global, css } from "@emotion/core"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Global
+        styles={css`
+          html {
+            overflow-y: scroll;
+          }
+        `}
+      />
       <Box mx="auto" maxWidth={960} p={1}>
         <Header />
         <main>{children}</main>
