f96422dd99ef7dab397c051f0b962133f4aae7d5
mravand <frransier@gmail.com>
2020-01-15 01:13:38 +0100

    sm

--- site/src/components/nav.js
+++ site/src/components/nav.js
@@ -13,7 +13,7 @@ const Nav = () => {
       }}
     >
       <Link to="/nyheter" style={{ textDecoration: "none" }}>
-        <Styled.h2 sx={{ mx: 7 }}>Nyheter</Styled.h2>
+        <Styled.h2 sx={{ mx: 8 }}>Nyheter</Styled.h2>
       </Link>
       <div sx={{ mx: "auto" }}></div>
       <div sx={{ mx: [6, 10] }}>
@@ -23,7 +23,7 @@ const Nav = () => {
       </div>
       <div sx={{ mx: "auto" }}></div>
       <Link to="/fantasy" style={{ textDecoration: "none" }}>
-        <Styled.h2 sx={{ mx: 7 }}>Fantasy</Styled.h2>
+        <Styled.h2 sx={{ mx: 8 }}>Fantasy</Styled.h2>
       </Link>
     </div>
   )
