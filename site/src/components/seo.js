c5f03757d0e9808a1688ffa0b4e4428f8290b6a8
mravand <frransier@gmail.com>
2020-01-17 19:45:43 +0100

    test

--- site/src/components/seo.js
+++ site/src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import logo from "../images/metaImage.jpg"
+import logo from "../images/metaImage.png"
 
 const SEO = ({ description, meta, title, image }) => {
   const { site } = useStaticQuery(
