94b4cb13eed0780c4750ad48ddb743654756a71b
mravand <frransier@gmail.com>
2020-01-19 14:13:03 +0100

    sm

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icon-v1.svg`,
+        icon: `src/images/manifest.svg`,
       },
     },
     `gatsby-plugin-offline`,
