b1099455158dc99fe2e9ccd10bae214ed4c37adf
frransier <frransier@gmail.com>
2020-05-25 22:32:02 +0200

    init

--- site/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "typeface-alfa-slab-one"
-import "typeface-cabin"
-import "typeface-roboto-condensed"
-
-import { Provider } from "./src/state/"
-
-export const wrapRootElement = ({ element }) => <Provider>{element}</Provider>
