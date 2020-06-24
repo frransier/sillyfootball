0d607bfe5e710ccb7e6e3ab642194ea4a16a8e62
mr <frransier>
2020-06-24 12:09:35 +0200

    m

--- site/src/components/footer.js
+++ site/src/components/footer.js
@@ -12,155 +12,155 @@ const Footer = () => {
   const dispatch = useGlobalDispatch()
 
   return (
-    <footer
-      sx={{
-        display: "grid",
-        gridTemplateColumns: ["12% 45% 30% 13%", "8% 64% 20% 8%"],
-        alignItems: "center",
-        justifyItems: "center",
-        bg: "bg",
-        pt: 5,
-        mx: 4,
-        mb: 6
-      }}
-    >
-      <Link
-        to="/"
+    <footer sx={{ height: 100, my: 6 }}>
+      <div
         sx={{
-          textDecoration: "none",
-          color: "white"
-          // justifySelf: "start"
+          display: "grid",
+          gridTemplateColumns: ["12% 45% 30% 13%", "8% 64% 20% 8%"],
+          alignItems: "center",
+          justifyItems: "center",
+          bg: "bg",
+          mx: 4
         }}
       >
-        {/* <Frame borderWidth={0} borderRadius={999}> */}
-        <div
+        <Link
+          to="/"
           sx={{
-            display: "grid",
-            bg: "background",
-            color: "secondary",
-            width: 35,
-            height: 35,
-            borderRadius: 999
+            textDecoration: "none",
+            color: "white"
+            // justifySelf: "start"
           }}
         >
-          <img
-            src={logo}
-            alt="Silly Football Logo"
-            sx={{ width: 23, alignSelf: "center", justifySelf: "center" }}
-          />
-        </div>
-        {/* </Frame> */}
-      </Link>
-      <Styled.h1
-        sx={{
-          m: 0,
-          mx: 3,
-          color: ["background", "background"],
-          fontSize: 3,
-          justifySelf: "start",
-          alignSelf: "center"
+          {/* <Frame borderWidth={0} borderRadius={999}> */}
+          <div
+            sx={{
+              display: "grid",
+              bg: "background",
+              color: "secondary",
+              width: 35,
+              height: 35,
+              borderRadius: 999
+            }}
+          >
+            <img
+              src={logo}
+              alt="Silly Football Logo"
+              sx={{ width: 23, alignSelf: "center", justifySelf: "center" }}
+            />
+          </div>
+          {/* </Frame> */}
+        </Link>
+        <Styled.h1
+          sx={{
+            m: 0,
+            mx: 3,
+            color: ["background", "background"],
+            fontSize: 3,
+            justifySelf: "start",
+            alignSelf: "center"
 
-          // textShadow: "1px 1px 0px primary"
-        }}
-      >
-        SILLY FOOTBALL
-      </Styled.h1>
+            // textShadow: "1px 1px 0px primary"
+          }}
+        >
+          SILLY FOOTBALL
+        </Styled.h1>
 
-      <Link
-        to="/livescore/"
-        activeClassName="active"
-        sx={{
-          textDecoration: "none",
-          textAlign: "center",
-          color: "muted",
-          bg: "background",
-          p: 3,
-          alignSelf: "center",
-          // mx: ["", 4],
-          justifySelf: ["center", "end"],
-          borderRadius: 3,
-          width: "100%"
-
-          // "&.active": {
-          //   color: "red"
-          // }
-        }}
-        onClick={() => dispatch({ type: "set-loading", payload: true })}
-      >
-        <Styled.h5 sx={{ m: 0, fontWeight: 700 }}>Livescore</Styled.h5>
-      </Link>
-      {state && state.user && (
         <Link
-          to="/account/"
+          to="/livescore/"
           activeClassName="active"
           sx={{
             textDecoration: "none",
-            color: "text"
+            textAlign: "center",
+            color: "muted",
+            bg: "background",
+            p: 3,
+            alignSelf: "center",
+            // mx: ["", 4],
+            justifySelf: ["center", "end"],
+            borderRadius: 3,
+            width: "100%"
+
             // "&.active": {
             //   color: "red"
             // }
           }}
           onClick={() => dispatch({ type: "set-loading", payload: true })}
         >
-          <div
-            sx={{
-              display: "grid",
-              bg: "background",
-              color: "primary",
-              // boxShadow: "1px 1px 4px darkgrey",
-              width: 28,
-              height: 28,
-              borderRadius: 999
-            }}
-          >
-            <RiHome2Line
-              sx={{
-                alignSelf: "center",
-                // mx: [4],
-                mb: 1,
-                justifySelf: ["center"]
-              }}
-              size={20}
-            />
-          </div>
+          <Styled.h5 sx={{ m: 0, fontWeight: 700 }}>Livescore</Styled.h5>
         </Link>
-      )}
-      {state && !state.user && (
-        <button
-          sx={{
-            cursor: "pointer",
-            appearance: "none",
-            outline: "none",
-            border: "none",
-            bg: "white",
-            textDecoration: "none",
-            color: "secondary"
-          }}
-          aria-label="Login"
-          onClick={() => loginWithRedirect({})}
-        >
-          <div
+        {state && state.user && (
+          <Link
+            to="/account/"
+            activeClassName="active"
             sx={{
-              display: "grid",
-              bg: "secondary",
-              color: "primary",
-              boxShadow: "1px 1px 4px darkgrey",
-              width: 28,
-              height: 28,
-              borderRadius: 999
+              textDecoration: "none",
+              color: "text"
+              // "&.active": {
+              //   color: "red"
+              // }
             }}
+            onClick={() => dispatch({ type: "set-loading", payload: true })}
           >
-            <RiHome2Line
+            <div
               sx={{
-                alignSelf: "center",
-                // mx: [4],
-                justifySelf: ["center"]
+                display: "grid",
+                bg: "background",
+                color: "primary",
+                // boxShadow: "1px 1px 4px darkgrey",
+                width: 28,
+                height: 28,
+                borderRadius: 999
               }}
-              size={20}
-            />
-          </div>
-        </button>
-      )}
+            >
+              <RiHome2Line
+                sx={{
+                  alignSelf: "center",
+                  // mx: [4],
+                  mb: 1,
+                  justifySelf: ["center"]
+                }}
+                size={20}
+              />
+            </div>
+          </Link>
+        )}
+        {state && !state.user && (
+          <button
+            sx={{
+              cursor: "pointer",
+              appearance: "none",
+              outline: "none",
+              border: "none",
+              bg: "white",
+              textDecoration: "none",
+              color: "secondary"
+            }}
+            aria-label="Login"
+            onClick={() => loginWithRedirect({})}
+          >
+            <div
+              sx={{
+                display: "grid",
+                bg: "secondary",
+                color: "primary",
+                boxShadow: "1px 1px 4px darkgrey",
+                width: 28,
+                height: 28,
+                borderRadius: 999
+              }}
+            >
+              <RiHome2Line
+                sx={{
+                  alignSelf: "center",
+                  // mx: [4],
+                  justifySelf: ["center"]
+                }}
+                size={20}
+              />
+            </div>
+          </button>
+        )}
+      </div>
     </footer>
   )
 }
